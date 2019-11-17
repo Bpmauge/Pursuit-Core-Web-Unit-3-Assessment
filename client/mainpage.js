c5535ea968e5077fc63d35219c5da926912d0db2
Bpmauge <briahanamauge@pursuit.org>
2019-11-17 15:16:31 -0500

    everything works correctly

--- client/mainpage.js
+++ client/mainpage.js
@@ -1,19 +1,87 @@
 document.addEventListener('DOMContentLoaded', () => {
     let sightingForm = document.querySelector('#sightingForm');
-        sightingForm.addEventListener('change', (event) => {
-        console.log(`button was changed to ${event.target.value}`)
+    sightingForm.addEventListener('change', (event) => {
+        console.log(`button was changed to ${event.target.value}`);
+        selectedButton(event.target.value);
+
     })
 
     let researcherForm = document.querySelector('#researcherForm');
-researcherForm.addEventListener('change', (event) => {
-    console.log(`drop was changed to ${event.target.value}`)
+    researcherForm.addEventListener('change', (event) => {
+        console.log(`drop was changed to ${event.target.value}`);
+        getResearcherSighting(event.target.value);
+    })
 })
-})
+const selectedButton = (button) => {
+    let value = button;
+    let researcherForm = document.querySelector('#researcherForm');
+    if (value === 'all') {
+        researcherForm.style.visibility = 'hidden';
+        getAllSighting();
+    } else {
+        makeDropDown();
+        researcherForm.style.visibility = 'visible';
 
+    }
+}
 
-const getResearcherSighting = async () =>{
-    response = await axios.get(``)
+const getAllSighting = async (event) => {
+    response = await axios.get(`http://localhost:2591/sightings/`)
+    let all = response.data.payload;
+    console.log(all)
+    let sightingOL = document.querySelector('#sightings');
+    sightingOL.innerText = '';
+    let vowels = ['A', 'E', 'I', 'O', 'U'];
+    all.forEach((single) => {
+        let listItem = document.createElement('li');
 
+        if (vowels.includes(single.species_name[0])) {
+            listItem.innerText = `An ${single.species_name} was spotted by ${single.researcher_name} in the ${single.category}.`;
+        }
+        else {
+            listItem.innerText = `A ${single.species_name} was spotted by ${single.researcher_name} in the ${single.category}.`;
+
+        }
+        sightingOL.append(listItem);
+    });
+}
+
+const makeDropDown = async () => {
+    let sightingUL = document.querySelector('#sightings');
+    sightingUL.innerText = '';
+
+    let select = document.querySelector('#researchers');
+
+    response = await axios.get(`http://localhost:2591/researchers/`)
+    let researchers = response.data.payload;
+
+    researchers.forEach((researcher) => {
+        let option = document.createElement('option');
+        option.setAttribute("value", `${researcher.id}`/*`${index}`*/)
+        option.innerText = researcher.name;
+        select.add(option)
+    })
+}
+const getResearcherSighting = async (researcher_id) =>{
+    let sightingOL = document.querySelector('#sightings');
+    sightingOL.innerText = '';
+
+response = await axios.get(`http://localhost:2591/sightings/researchers/${researcher_id}`);
+let all = response.data.payload;
+// console.log(all)
+let vowels = ['A', 'E', 'I', 'O', 'U'];
+    all.forEach((single) => {
+        let listItem = document.createElement('li');
+
+        if (vowels.includes(single.species_name[0])) {
+            listItem.innerText = `An ${single.species_name} was spotted by ${single.researcher_name} in the ${single.category}.`;
+        }
+        else {
+            listItem.innerText = `A ${single.species_name} was spotted by ${single.researcher_name} in the ${single.category}.`;
+
+        }
+        sightingOL.append(listItem);
+    });
 }
 
 
--- server/routes/sightings.js
+++ server/routes/sightings.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 const getAllSightings = async (req, res) => {
     try{
-    let selectQuery = `SELECT * FROM sightings`
+    let selectQuery = `SELECT researcher_id, species_id, habitat_id, researchers.id AS researcher_id, researchers.name AS researcher_name, species.id AS species_id, species.name AS species_name, habitats.id AS habitat_id, habitats.category AS category  FROM sightings JOIN researchers ON sightings.researcher_id = researchers.id JOIN species ON sightings.species_id = species.id JOIN habitats ON sightings.habitat_id = habitats.id;`
     let results = await db.any(selectQuery);
     res.status(200).json({
         status: 'success',
@@ -42,7 +42,7 @@ const getSpeciesSightings = async (req, res) => {
 
 const getResearcherSightings = async (req, res) => {
     try{
-    let selectQuery = `SELECT * FROM sightings WHERE researcher_id = $1;`
+    let selectQuery = `SELECT researcher_id, species_id, habitat_id, researchers.id AS researcher_id, researchers.name AS researcher_name, species.id AS species_id, species.name AS species_name, habitats.id AS habitat_id, habitats.category AS category  FROM sightings JOIN researchers ON sightings.researcher_id = researchers.id JOIN species ON sightings.species_id = species.id JOIN habitats ON sightings.habitat_id = habitats.id WHERE researcher_id = $1;`
     let results = await db.any(selectQuery, parseInt(req.params.researcher_id));
     res.status(200).json({
         status: 'success',
